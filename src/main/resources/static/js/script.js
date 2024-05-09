5473fea65ea21ff03fc0f78b7b3faf3e5c0b6223
Johoseong <whghtjd320@naver.com>
2024-05-10 04:52:45 +0900

    feat: 구 선택메뉴 html 개발

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,22 @@
+const indicator = document.querySelector('.nav-indicator');
+const items = document.querySelectorAll('.gu-menu-item');
+
+function handleIndicator(el) {
+  items.forEach(item => {
+    item.classList.remove('gu-menu-active');
+    item.removeAttribute('style');
+  });
+
+  indicator.style.width = `${el.offsetWidth}px`;
+  indicator.style.left = `${el.offsetLeft}px`;
+  indicator.style.backgroundColor = el.getAttribute('active-color');
+
+  el.classList.add('menu-active');
+  el.style.color = el.getAttribute('active-color');
+}
+
+
+items.forEach((item, index) => {
+  item.addEventListener('click', e => {handleIndicator(e.target);});
+  item.classList.contains('menu-active') && handleIndicator(item);
+});
