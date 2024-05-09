b7d14a50c332f34fe671a2e06c2c293f99cb79a8
Johoseong <whghtjd320@naver.com>
2024-05-10 03:55:20 +0900

    refact: 미사용 import 제거

--- src/main/java/com/artrium/demo/controller/WebController.java
+++ src/main/java/com/artrium/demo/controller/WebController.java
@@ -1,7 +1,6 @@
 package com.artrium.demo.controller;
 
 import com.artrium.demo.dto.CultureInfo;
-import com.artrium.demo.repository.Culture;
 import com.artrium.demo.service.CultureService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,12 +9,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @Slf4j
 @Controller
